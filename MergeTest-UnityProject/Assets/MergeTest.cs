5db76e77c0fcecf8e98b3b15caeadab4aafc719b
Go <go05sato@gmail.com>
2017-12-09 17:32:53 +0900

    変数と関数追加

--- MergeTest-UnityProject/Assets/MergeTest.cs
+++ MergeTest-UnityProject/Assets/MergeTest.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class MergeTest : MonoBehaviour
 {
     private bool _isTest = true;
+    private int _maxNum = 5;
 
     private void TestA()
     {
         Debug.Log("TestA");
+    }
+
+    private void TestB()
+    {
+        for(int i = 0; i < _maxNum; i++)
+        {
+            Debug.Log(i);
+        }
     }
 }
