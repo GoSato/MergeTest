6815340297b76d9cd2666a44c04016d4c1ba0bc4
Go <go05sato@gmail.com>
2017-12-09 17:28:25 +0900

    検証用スクリプト追加

---
+++ MergeTest-UnityProject/Assets/MergeTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MergeTest : MonoBehaviour
+{
+    private bool _isTest = true;
+
+    private void TestA()
+    {
+        Debug.Log("TestA");
+    }
+}
